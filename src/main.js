6f105db1fa16af5e59833f4d7c85a130d6fbd234
Carl Cassar <carlcassar@gmail.com>
2020-03-28 15:32:05 +0000

    Add the ability to install the library as a plugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '@/assets/css/tailwind.css'
+import '@/assets/css/tailwind.css';
 
 import Input from './components/Input';
 
@@ -6,3 +6,24 @@ export {
     Input
 };
 
+const components = [
+    Input
+];
+
+const defaultOptions = {
+    prefix: 'tw-'
+};
+
+const TailwindVue = {
+    install(Vue, options = defaultOptions) {
+        components.forEach(component => Vue.component(options.prefix + toKebabCase(component.name), component));
+    }
+};
+
+export default TailwindVue;
+
+const toKebabCase = string => {
+    return string && string.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map(x => x.toLowerCase())
+        .join('-');
+};
