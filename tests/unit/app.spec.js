6e5ab439f8a5beaba323794d6df5c5d70d77dea2
Carl Cassar <carlcassar@gmail.com>
2020-04-01 15:02:10 +0100

    Add another app component test

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -3,4 +3,14 @@ import App from 'Components/App';
 
 describe('App', () => {
     it('renders the app component', () => expect(shallowMount(App).contains('div')).toBe(true));
+
+    it('renders the default slot', () => {
+        const wrapper = shallowMount(App, {
+            slots: {
+                default: 'Some content here'
+            }
+        });
+
+        expect(wrapper.vm.$el.innerHTML).toContain('Some content here');
+    });
 });
