03668500549147ebbd52e7903633f7b51ae9d728
Carl Cassar <carlcassar@gmail.com>
2020-04-01 11:33:15 +0100

    Add first component test

---
+++ tests/unit/panel.spec.js
@@ -0,0 +1,44 @@
+import { shallowMount } from '@vue/test-utils';
+import Panel from '@/components/Panel';
+
+describe('Panel', () => {
+    it('renders the panel', () => expect(shallowMount(Panel).contains('div')).toBe(true));
+
+    it('renders the default slot', () => {
+        const wrapper = shallowMount(Panel, {
+            slots: {
+                default: 'Some content here'
+            }
+        });
+
+        expect(wrapper.vm.$el.innerHTML).toContain('Some content here');
+    });
+
+    it('renders the header slot', () => {
+        const wrapper = shallowMount(Panel, {
+            slots: {
+                header: 'Header content here'
+            }
+        });
+
+        expect(wrapper.vm.$el.innerHTML).toContain('Header content here');
+    });
+
+
+    it('applies the correct styles', () => {
+        const panelClasses = 'panel-classes';
+        const headerClasses = 'header-classes';
+
+        const wrapper = shallowMount(Panel, {
+            propsData: {
+                theme: {
+                    panel: panelClasses,
+                    header: headerClasses
+                }
+            }
+        });
+
+        expect(wrapper.classes()).toContain(panelClasses);
+        expect(wrapper.find('header').classes()).toContain(headerClasses);
+    });
+});
