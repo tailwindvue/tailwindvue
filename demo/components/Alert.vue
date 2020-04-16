9648e5ddd9ac684f29735e18822c3d4a7f2a3a10
Carl Cassar <carlcassar@gmail.com>
2020-04-16 17:05:25 +0100

    Add component demo capability

---
+++ demo/components/Alert.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="content">
+        <h1 class="text-3xl">
+            Alert
+        </h1>
+
+        <tw-alert>An alert</tw-alert>
+
+        <tw-alert type="info">
+            An info alert
+        </tw-alert>
+
+        <tw-alert type="warning">
+            A warning alert
+        </tw-alert>
+
+        <tw-alert type="danger">
+            A danger alert
+        </tw-alert>
+
+        <tw-alert type="success">
+            A success alert
+        </tw-alert>
+
+        <tw-alert type="info">
+            <template #icon>
+                <i class="fa fa-pencil" />
+            </template>
+
+            An alert with an icon
+        </tw-alert>
+
+        <tw-alert :dismissable="true">
+            A dismissable alert
+        </tw-alert>
+
+        <tw-alert :duration="2">
+            A self dismissing alert
+        </tw-alert>
+    </div>
+</template>
+
+<script>
+    import Alert from '../../src/components/Alert';
+
+    export default {
+        components: {
+            'tw-alert': Alert
+        },
+    };
+</script>
+
+<style>
+    #content > * {
+        margin-bottom: 20px;
+    }
+</style>
